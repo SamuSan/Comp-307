a0de45abdbfb413a5a05ba3a0596a60f5bd3c4f9
samminns <samminns@10.140.73.239>
2014-03-20 18:36:00 +1300

    KFold kind of working

--- AssignmentOne_PartOne/Copy of COMP307_Assignment_One_part1/src/part1/KCrossFoldValidation.java
+++ AssignmentOne_PartOne/Copy of COMP307_Assignment_One_part1/src/part1/KCrossFoldValidation.java
@@ -12,88 +12,84 @@ public class KCrossFoldValidation {
 
 	private static ArrayList<Iris> flowers = new ArrayList<Iris>();
 	private static ArrayList<Iris> trainers = new ArrayList<Iris>();
-	private static int K =1;
-	public KCrossFoldValidation(ArrayList<Iris> flowers){
+	private static int K = 3;
+
+	public KCrossFoldValidation(ArrayList<Iris> flowers) {
 		this.flowers = flowers;
 		doKCrossFold();
 	}
-	
-	private static void doKCrossFold(){
-		
+
+	private static void doKCrossFold() {
+
 		ArrayList<Iris> test = new ArrayList<Iris>(flowers);
-		double [] successRates = new double[500];
+		ArrayList<Double> successRates = new ArrayList<Double>();
 		ArrayList<ArrayList<Iris>> sets = new ArrayList<ArrayList<Iris>>();
 		ArrayList<Pair> candidates = new ArrayList<Pair>();
 		double count = 0;
 		double success = 0;
-		int numCases=flowers.size();
-		int k = getK(numCases);
-		int numElements=numCases / k;
+		int numCases = flowers.size();
+		int numSplits = getK(numCases);
+		int numElements = numCases / numSplits;
 		int incr = numElements;
 		d(numElements);
 		int start = 0;
-		for (int i = 1; i <=k; i++) {
+		for (int i = 1; i <= numSplits; i++) {
 			ArrayList<Iris> sample = new ArrayList<Iris>();
-			//Add the flowers to this
-			for(;start<numElements;start++){
+			// Add the flowers to this
+			for (; start < numElements; start++) {
 				sample.add(test.get(start));
 			}
-			numElements+=incr;
+			numElements += incr;
 			sets.add(sample);
 		}
-		
-		int testSet =0;
-		
-		//Take one set of data from the asmple sets
-		//use this as the training data
-		//Take the remaining sets and use as testing data
-		
-		
-		for (int i = testSet; i < k; i++) {
-			ArrayList<Iris> trainingSet = sets.get(i);
-			for (int j = testSet; i < k; i++) {
-				if(i != j){
+
+		int testSet = 0;
+
+		// Take one set of data from the asmple sets
+		// use this as the training data
+		// Take the remaining sets and use as testing data
+int cases =0;
+		for (int i = 0; i < numSplits; i++) {
+			ArrayList<Iris> trainingSet = sets.get(testSet);
+			for (int j = 0; j < numSplits; j++) {
+				if (i != j) {
+					d(i + " " + j);
 					ArrayList<Iris> testingSet = sets.get(j);
-					
-					
 					for (Iris x : testingSet) {
-					for (Iris e : trainingSet) {
-						double dist = EucledianCalc.euclidCalc(x, e);
-						// d("Dist = " + dist);
-						Pair p = new Pair(dist, e);
-						candidates.add(p);
+						for (Iris e : trainingSet) {
+							double dist = EucledianCalc.euclidCalc(x, e);
+							Pair p = new Pair(dist, e);
+							candidates.add(p);
+						}
+						Collections.sort(candidates, new PairComparator());
+						// d(x.toString());
+						ArrayList<Pair> sublist = new ArrayList<Pair>();
+						for (int g = 0; g < K; g++) {
+							sublist.add(candidates.get(g));
+						}
+						String match = doVote(sublist);
+						
+						if (x.getClazz().equals(match)) {
+							d("Match");
+							cases++;
+							success++;
+						}
+						else{
+							cases++;
+							d("No Match");
+						}
 					}
-					Collections.sort(candidates, new PairComparator());
-				//	d(candidates.size());
-					d(x.toString());
-					ArrayList<Pair> sublist = new ArrayList<Pair>();
-					for (int g = 0; g < K; g++) {
-						sublist.add(candidates.get(g));
-					}
-					String match = doVote(sublist);
-					System.out.println(match);
-					//d(match);
-					count++;
-					//d("Chosen Dist = " + candidates.get(0).dist);
-					if (x.getClazz().equals(match)) {
-						success++;
-					}
-//					if (i.getClass() == candidates.get(0).i.getClass()) {
-//						success++;
-//					}
-					// d("///////////////////////////S");
-					candidates.clear();
-				}
-				double rate = success / count;
-				successRates[i] = rate;
-				d(rate + "% success");
+
 				}
 
 			}
+			d("Cases"+cases);
+			testSet++;
+			successRates.add(success);
+			d("Sucess"+success);
+			//cases=0;
 		}
-	for (int i = 0; i < successRates.length; i++) {
-		d(successRates[i]);
-	}
+		d(successRates.size());
 	}
 
 	private static String doVote(List<Pair> list) {
@@ -123,19 +119,19 @@ public class KCrossFoldValidation {
 
 		return choice;
 	}
-	private static  int getK(int numCases) {
-		int max =0;
+
+	private static int getK(int numCases) {
+		int max = 0;
 		for (int i = 2; i < 10; i++) {
-			if(numCases % i ==0 && i > max){
+			if (numCases % i == 0 && i > max) {
 				max = i;
 			}
 		}
 		return max;
 	}
-	
-private static void d(Object o) {
-	System.out.println(o);
-}
 
-	
+	private static void d(Object o) {
+		System.out.println(o);
+	}
+
 }
